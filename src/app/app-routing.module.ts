3d5877986ad74728f958640d44bb3315aed167f5
guillerdguez <guillermordguezrdguez@gmail.com>
2025-03-14 09:41:13 +0100

    limpieza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
- 
 import { HomeComponent } from './UI/home/home.component';
 import { EsquemaListaComponent } from './UI/esquema-lista/esquema-lista.component';
 import { LoginComponent } from './UI/clientes/seguridad/login/login.component';
 import { CartComponent } from './UI/compra/cart/cart.component';
 import { CheckoutComponent } from './UI/compra/checkout/checkout.component';
- 
+
 import { ProductoDetailComponent } from './UI/productos/producto-editar/producto-detail.component';
 import { PedidoDeailComponent } from './UI/compra/pedido/pedido-deail/pedido-deail.component';
 import { UserDetailComponent } from './UI/clientes/user-editar/user-detail.component';
- 
+
 import { FormularioComponentProducto } from './UI/productos/formularioProductos/formulario.component';
 import { FormularioComponentUser } from './UI/clientes/formularioProductos/formulario.component';
- 
+
 import { authGuard } from './Service/seguridad/AuthGuard';
 import { roleGuard } from './Service/seguridad/RoleGuard';
 import { UserAuthority } from './Model/Domain/User/UserAuthority.enum';
 
 const routes: Routes = [
- 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'tabla/:tipo', component: EsquemaListaComponent },
   { path: 'productos', redirectTo: '/tabla/productos' },
   { path: 'favorito', redirectTo: '/tabla/favorito' },
   { path: 'ofertas', redirectTo: '/tabla/ofertas' },
@@ -30,15 +30,14 @@ const routes: Routes = [
   { path: 'users', redirectTo: '/tabla/users' },
   { path: 'pedidosCliente', redirectTo: '/tabla/pedidosCliente' },
   { path: 'pedidos', redirectTo: '/tabla/pedidos' },
- 
+
   { path: 'home', component: HomeComponent },
-  { path: 'tabla/:tipo', component: EsquemaListaComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: FormularioComponentUser },
   { path: 'checkout', component: CheckoutComponent },
- 
+
   { path: 'carrito', component: CartComponent, canMatch: [authGuard] },
- 
+
   {
     path: 'detail/Productos/:id',
     component: ProductoDetailComponent,
@@ -52,7 +51,7 @@ const routes: Routes = [
     component: UserDetailComponent,
     canMatch: [authGuard],
   },
- 
+
   {
     path: 'newProducto',
     component: FormularioComponentProducto,
